fa1746d288a7656936fc33e00857258882fa261b
adrpadua <adrpadua@gmail.com>
2016-11-09 09:03:01 -0800

    Created Project Files

---
+++ MedAppJam16/MedAppJam16/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MedAppJam16
+//
+//  Created by Adrian Padua on 11/9/16.
+//  Copyright © 2016 Adrian Padua. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
