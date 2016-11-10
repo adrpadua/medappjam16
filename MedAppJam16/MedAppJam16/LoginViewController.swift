0bdac1259c5590e23f9b01195e20d198397791bc
adrpadua <adrpadua@gmail.com>
2016-11-09 22:52:22 -0800

    Work on login screen

---
+++ MedAppJam16/MedAppJam16/LoginViewController.swift
@@ -0,0 +1,45 @@
+//
+//  LoginViewController.swift
+//  MedAppJam16
+//
+//  Created by Adrian Padua on 11/9/16.
+//  Copyright © 2016 Adrian Padua. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginBtnPressed(_ sender: Any) {
+        
+    }
+    
+    func loginWithCredentials(emailAddress: String, password: String) {
+        
+        
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
