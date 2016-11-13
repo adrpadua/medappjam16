398e756d67fbfa7bf03bc7b206985837f3e0fee9
adrpadua <adrpadua@gmail.com>
2016-11-12 16:57:36 -0800

    Fixed things, added treatment and symptom cells, added images

--- MedAppJam16/MedAppJam16/DashboardViewController.swift
+++ MedAppJam16/MedAppJam16/DashboardViewController.swift
@@ -7,64 +7,28 @@
 //
 
 import UIKit
-import JTAppleCalendar
 
 class DashboardViewController: UIViewController {
     
     @IBOutlet weak var doctorImageView: UIImageView!
-    @IBOutlet weak var calendarView: JTAppleCalendarView!
-    @IBOutlet weak var monthLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        calendarView.dataSource = self
-        calendarView.delegate = self
-        calendarView.registerCellViewXib(file: "CellView") // Registering your cell is manditory
-        calendarView.cellInset = CGPoint(x: 0, y: 0)       // default is (3,3)
-        
+        doctorImageView.layer.masksToBounds = false
         doctorImageView.layer.cornerRadius = 5.0
+        doctorImageView.clipsToBounds = true
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-}
-
-extension DashboardViewController: JTAppleCalendarViewDataSource, JTAppleCalendarViewDelegate {
-    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy MM dd"
-        
-        let startDate = formatter.date(from: "2016 02 01")! // You can use date generated from a formatter
-        let endDate = Date()                                // You can also use dates created from this function
-        let parameters = ConfigurationParameters(startDate: startDate,
-                                                 endDate: endDate,
-                                                 numberOfRows: 6,
-                                                 calendar: Calendar.current,
-                                                 generateInDates: .forAllMonths,
-                                                 generateOutDates: .tillEndOfGrid,
-                                                 firstDayOfWeek: .sunday)
-        return parameters
-    }
     
-    func calendar(_ calendar: JTAppleCalendarView, willDisplayCell cell: JTAppleDayCellView, date: Date, cellState: CellState) {
-        
-        let black = UIColor.black
-        let gray = UIColor.gray
-        let myCustomCell = cell as! CellView
-        
-        // Setup Cell text
-        myCustomCell.dayLabel.text = cellState.text
-        
-        // Setup text color
-        if cellState.dateBelongsTo == .thisMonth {
-            myCustomCell.dayLabel.textColor = UIColor(colorWithHexValue: 0x56279C)
-        } else {
-            myCustomCell.dayLabel.textColor = UIColor(colorWithHexValue: 0x090C39)
-        }
+    @IBAction func phoneNumberBtnPressed(_ sender: Any) {
+        print("Calling Doctor Chan!")
     }
 }
 
